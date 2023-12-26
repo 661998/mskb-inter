c3f6d433abdd5a4f951bbee3a661378fe236edf0
Saurabh Kumar <saurabhkumar6698@gmail.com>
2023-12-26 18:32:18 +0530

    payment gateway testling

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -16,24 +16,6 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 // $settings['skip_permissions_hardening'] = TRUE;
 
 /**
-* Set session lifetime (in seconds), i.e. the time from the user's last visit
-* to the active session may be deleted by the session garbage collector. When
-* a session is deleted, authenticated users are logged out, and the contents
-* of the user's $_SESSION variable is discarded.
-*/
-ini_set('session.gc_maxlifetime', 200000);
-
-/**
-* Set session cookie lifetime (in seconds), i.e. the time from the session is
-* created to the cookie expires, i.e. when the browser is expected to discard
-* the cookie. The value 0 means "until the browser is closed".
-*/
-ini_set('session.cookie_lifetime', 2000000);
-
-ini_set('session.cookie_samesite', 'none');
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_path', '/; samesite=None');
-/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
