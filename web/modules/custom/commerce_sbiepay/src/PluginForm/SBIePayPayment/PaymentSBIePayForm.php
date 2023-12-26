1c7221b68010b5f20f111cb94425ec2251aa44e3
Saurabh Kumar <saurabhkumar6698@gmail.com>
2023-12-26 15:03:06 +0530

    payment gateway testling

--- web/modules/custom/commerce_sbiepay/src/PluginForm/SBIePayPayment/PaymentSBIePayForm.php
+++ web/modules/custom/commerce_sbiepay/src/PluginForm/SBIePayPayment/PaymentSBIePayForm.php
@@ -45,7 +45,7 @@ class PaymentSBIePayForm extends BasePaymentOffsiteForm {
     $cur = $payment_gateway_plugin->getConfiguration()['currency'];
     $lng = $payment_gateway_plugin->getConfiguration()['language'];
     $amt = round($payment->getAmount()->getNumber(), 2);
-    $redirect_url = Url::FromRoute('commerce_payment.checkout.return', [
+    $success_url = Url::FromRoute('commerce_payment.checkout.return', [
       'commerce_order' => $order_id,
       'step' => 'payment',
     ], ['absolute' => TRUE])->toString();
@@ -55,50 +55,26 @@ class PaymentSBIePayForm extends BasePaymentOffsiteForm {
     ], ['absolute' => TRUE])->toString();
 
     //requestparameter 
-    // $requestParameter = $merchant_id."|DOM|IN|".$cur."|".$amt."|Other|".$redirect_url."|".$cancel_url."|SBIEPAY|".$order_id."|2|NB|ONLINE|ONLINE";
-    $requestParameter = $merchant_id."|DOM|IN|".$cur."|".$amt."|Other|".$redirect_url."|".$cancel_url."|SBIEPAY|1144ID7Q9ML7|2|NB|ONLINE|ONLINE";
-
-    // $parameters = [
-    //   'merchant_id' => $merchant_id,
-    //   'order_id' => $order_id,
-    //   'tid' => time(),
-    //   'amount' => round($payment->getAmount()->getNumber(), 2),
-    //   'currency' => $payment->getAmount()->getCurrencyCode(),
-    //   'language' => $lng,
-    //   'redirect_url' => Url::FromRoute('commerce_payment.checkout.return', [
-    //     'commerce_order' => $order_id,
-    //     'step' => 'payment',
-    //   ], ['absolute' => TRUE])->toString(),
-    //   'cancel_url' => Url::FromRoute('commerce_payment.checkout.cancel', [
-    //     'commerce_order' => $order_id,
-    //     'step' => 'payment',
-    //   ], ['absolute' => TRUE])->toString(),
-    //   'billing_name' => isset($address) ? $address->getGivenName() : "",
-    //   'billing_address' => isset($address) ? $address->getAddressLine1() : "",
-    //   'billing_city' => isset($address) ? $address->getLocality() : "",
-    //   'billing_state' => isset($address) ? $address->getAdministrativeArea() : "",
-    //   'billing_country' => isset($address) ? \Drupal::service('country_manager')->getList()[$address->getCountryCode()]->__toString() : "",
-    //   'billing_zip' => isset($address) ? $address->getPostalCode() : "",
-    //   'billing_email' => isset($address) ? $order->getEmail() : "",
-    //   'billing_tel' => $billing_profile->field_mobile->value,
-    //   'billing_notes' => $notes
-    // ];
-
-    // $merchantData = '';
-    // foreach ($parameters as $key => $value) {
-    //   $merchantData .= $key . '=' . $value . '&';
-    // }
+    $orderid = $order_id;
+    for ($i=0; $i<10; $i++)
+    {
+        $d=rand(1,30)%2;
+        $d=$d ? chr(rand(65,90)) : chr(rand(48,57));
+        $orderid=$orderid.$d;
+    }
+    $requestParameter = $merchant_id."|DOM|IN|".$cur."|".$amt."|Other|".$success_url."|".$cancel_url."|SBIEPAY|".$orderid."|2|NB|ONLINE|ONLINE";
+    // $requestParameter = $merchant_id."|DOM|IN|".$cur."|".$amt."|Other|".$success_url."|".$cancel_url."|SBIEPAY|11YK9JRA6F0P|2|NB|ONLINE|ONLINE";
 
     $encrypt = new SBIePayEncryption();
-    $parameters['EncryptTrans'] = $encrypt->encrypt($requestParameter, $working_key);
+    $parameters['EncryptTrans'] = $encrypt->encrypt($requestParameter, $merchant_key);
     $parameters['merchIdVal'] = $merchant_id;
+    $parameters['submit'] = 'Submit';
 
     if ($mode == 'test') {
       $redirect_url = self::CCAVENUE_TEST_URL;
     } else {
       $redirect_url = self::CCAVENUE_LIVE_URL;
     }
-    // dd($parameters['EncryptTrans']);
 
     return $this->buildRedirectForm($form, $form_state, $redirect_url, $parameters, $redirect_method);
   }
